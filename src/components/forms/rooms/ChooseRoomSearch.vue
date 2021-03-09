1dbe5679556b08b0c0fcc19e265cf824d9a276ed
Svein Jakob Høie <sveinjakobhoie@gmail.com>
2021-03-09 14:10:02 +0100

    work 09.03

--- src/components/forms/rooms/ChooseRoomSearch.vue
+++ src/components/forms/rooms/ChooseRoomSearch.vue
@@ -1,33 +1,68 @@
 <template>
     <base-card class="card">
-        <form>
-            <div>
+        <form @submit.prevent="search">
+            <div class="form-control">
                 <label>Bygg</label>
-                <select></select>
+                <select name="building" @change="handleChange">
+                    <option :value="null">Velg bygg</option>
+                    <option v-for="b in buildings" :key="b.id" :value="b.id">{{ b.name }}</option>
+                </select>
             </div>
-            <div>
+            <div class="form-control">
                 <label>Rom</label>
-                <select></select>
+                <select name="room" @change="handleChange" :disabled="!buildingId">
+                    <option :value="null">Velg rom</option>
+                    <option v-for="room in filteredRooms" :key="room.id" :value="room.id">{{ room.name }}</option>
+                </select>
             </div>
-            <div>
-                <base-button>Søk</base-button>
+            <div class="form-control">
+                <base-button>Gå</base-button>
             </div>
         </form>
     </base-card>
 </template>
 
+<script>
+export default {
+    data() {
+        return {
+            buildingId: null,
+            roomId: null,
+            buildings: [
+                { id: 0, name: 'A-Bygget' },
+                { id: 1, name: 'B-Bygget' }
+            ],
+            rooms: [
+                { id: 0, buildingId: 0, name: '301', seats: 8 },
+                { id: 1, buildingId: 0, name: '302', seats: 6 },
+                { id: 2, buildingId: 1, name: 'B-02', seats: 6 }
+            ]
+        };
+    },
+    computed: {
+        filteredRooms() {
+            return this.rooms.filter(({ buildingId }) => (this.buildingId ? buildingId === +this.buildingId : false));
+        }
+    },
+    methods: {
+        go() {},
+        handleChange({ target }) {
+            this[`${target.name}Id`] = target.value;
+        }
+    }
+};
+</script>
+
 <style scoped>
 .card {
-    margin: 0;
-    width: 50%;
-    min-width: 25rem;
-    max-width: 50rem;
+    margin: 2rem 0 1rem;
+    width: min-content;
 }
 
 form {
     display: flex;
-    justify-content: space-around;
-    align-items: center;
+    justify-content: center;
+    align-items: flex-end;
 }
 
 form div {
@@ -37,12 +72,19 @@ form div {
 }
 
 select {
-    min-width: 4rem;
+    padding: 0.3rem 0.2rem;
+    font-family: inherit;
+    min-width: 10rem;
+    font-size: 0.9rem;
 }
 
 label {
     text-transform: uppercase;
-    font-size: 0.8rem;
+    font-size: 1rem;
     font-weight: 500;
+    margin-bottom: 0.3rem;
+}
+.form-control {
+    margin: 0 1rem;
 }
 </style>
---
+++ src/components/forms/rooms/FindRoomSearch.vue
@@ -0,0 +1,65 @@
+<template>
+    <base-card class="card">
+        <form @submit.prevent="search">
+            <div class="form-control">
+                <label>Dato</label>
+                <input type="date" />
+            </div>
+            <div class="form-control">
+                <label>Fra kl.</label>
+                <input type="time" />
+            </div>
+            <div class="form-control">
+                <input type="time" />
+            </div>
+            <div class="form-control">
+                <label>Bygg</label>
+                <select></select>
+            </div>
+            <div class="form-control">
+                <base-button>Søk</base-button>
+            </div>
+        </form>
+    </base-card>
+</template>
+
+<style scoped>
+.card {
+    margin: 2rem 0 1rem;
+    width: min-content;
+}
+
+form {
+    display: flex;
+    justify-content: center;
+    align-items: flex-end;
+}
+
+form div {
+    display: flex;
+    flex-direction: column;
+    align-items: start;
+}
+
+select,
+input[type='date'],
+input[type='time'] {
+    padding: 0.3rem 0.2rem;
+    font-family: inherit;
+    font-size: 0.9rem;
+}
+
+select {
+    min-width: 10rem;
+}
+
+label {
+    text-transform: uppercase;
+    font-size: 1rem;
+    font-weight: 500;
+    margin-bottom: 0.3rem;
+}
+.form-control {
+    margin: 0 1rem;
+}
+</style>
