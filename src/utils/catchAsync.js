21721e4f79ab52a02e21c430cf66dc473909a1dc
hakon <haakaha@stud.ntnu.no>
2021-04-17 16:35:27 +0200

    added error handling for unable to connect to database

--- src/utils/catchAsync.js
+++ src/utils/catchAsync.js
@@ -3,6 +3,10 @@ export default fn => {
         try {
             return await fn.apply(this, arguments);
         } catch (error) {
+            if (!error.response) {
+                return { error: "Can't connect to database" };
+            }
+
             return { error: error.response.data.message };
         }
     };
